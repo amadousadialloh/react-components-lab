2c9104ce8164b862a6324232daf168c5f448e77f
amadousadialloh <amadousadialloh@gmail.com>
2024-12-03 21:19:27 -0600

    Display the weather forecast

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,17 @@
+import "./WeatherForecast.css";
+export const WeatherForecasts = (props) => {
+  const { day, img, imgAlt, conditions, time } = props;
+  return (
+    <div className="weather">
+      <h2>{day}</h2>
+      <img src={img} alt={imgAlt} />
+      <p>
+        <span>conditions: </span> {conditions}
+      </p>
+      <p>
+        <span>time: </span>
+        {time}
+      </p>
+    </div>
+  );
+};
